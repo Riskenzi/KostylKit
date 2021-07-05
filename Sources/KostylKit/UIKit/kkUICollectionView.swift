0e192cecb67affdd03e4b6c67e609127598b58e5
Valerii <melnykov.v@wouff.org>
2021-07-05 17:56:05 +0300

    update 0.0.1

--- Sources/KostylKit/UIKit/kkUICollectionView.swift
+++ Sources/KostylKit/UIKit/kkUICollectionView.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 extension UICollectionView {
-    func registerCellFromNib(_ nameCell: String) -> Void{
+   open func registerCellFromNib(_ nameCell: String) -> Void{
         let nib = UINib(nibName: nameCell, bundle: nil)
         self.register(nib, forCellWithReuseIdentifier: nameCell)
     }
